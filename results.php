764c9d075abe9bc9aea9047dfbd1366db41c9a8a
unknown <amr.ister20@gmail.com>
2019-02-18 20:43:44 +0200

    Second page is added

---
+++ results.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Api App</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <div class="results">
+      <div class="banners">
+        <div class="head text-center">
+          <h1>Cairo</h1>
+        </div>
+        <div class="images">
+          <div class="image">
+            <img src="" alt="">
+          </div>
+          <div class="image">
+            <img src="" alt="">
+          </div>
+          <div class="image">
+            <img src="" alt="">
+          </div>
+        </div>
+        <div class="images">
+          <div class="image">
+            <img src="" alt="">
+          </div>
+          <div class="image">
+            <img src="" alt="">
+          </div>
+          <div class="image">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="weather">
+        <div class="head text-center">
+          <h1>Wheather</h1>
+        </div>
+        <div class="details">
+          <ul>
+            <li>Temp: </li>
+            <li>Temp: </li>
+            <li>Temp: </li>
+          </ul>
+          <ul>
+            <li>Temp: </li>
+            <li>Temp: </li>
+            <li>Temp: </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
